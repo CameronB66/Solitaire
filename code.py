77c5e4ab22ebe41a2bcbf4e853ede388c6774646
CameronB66 <feed@cmbain.com>
2020-07-29 12:26:38 +0100

    rewriting to not be awful

--- code.py
+++ code.py
@@ -6,12 +6,22 @@ class Card:
 		self.val = val
 		self.num_val = {'A':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':11,'Q':12,'K':13}[val]
 		self.colour = {'S':'B','C':'B','D':'R','H':'R'}[suit]
-
+		self.on = None
+		
+	def goes_on(self, card):
+		if card == None:
+			return self.val == 'K'
+		
+		if card.colour != self.colour:
+			if self.num_val + 1 == card.num_val:
+				return True
+		
+		return False
 
 class Column:
 	def __init__(self):
-		self.cards_hidden = []
-		self.stack = []
+		self.cards_hidden = [None]
+		self.top = None
 
 	def deal(self, card):
 		self.cards_hidden.append(card)
@@ -19,37 +29,39 @@ class Column:
 	def turn(self):
 		card = self.cards_hidden[-1]
 		self.cards_hidden = self.cards_hidden[:-1]
-		self.stack = [card]
+		self.top = card
 
 class Deck_Turn:
 	def __init__(self):
-		self.cards = []
-		self.index = -1
-		self.card = None
-		self.size = 0
-
-	def add_card(self, card)
-		self.cards.append(card)
-		self.size += 1
+		self.deck = []
+		self.turned = []
 
 	def turn(self):
-		if self.size > 0:
-			if self.index + 3 < self.size:
-				self.index += 3
-				self.card = self.cards[index]
-			elif self.index == self.size -1:
-				self.index = -1
-				self.card = None
+		if len(self.deck) == 0:
+			if len(self.turned) > 0:
+				self.deck = self.turned
 				self.turn()
-			else:
-				self.index = self.size-1
-				self.card = self.cards[index]
+		else:
+			size = len(self.deck)
+			for i in range (min(3, size)):
+				card = self.deck[-1]
+				self.deck = self.deck[:-1]
+				self.turned.append(card)
+
+	def card(self):
+		if len(self.turned) == 0:
+			return None
+		else:
+			return self.turned[-1]
 
 	def take(self):
-		card = self.card
-		self.cards = self.cards[:index] + self.cards[index+1:]
-		self.index -= 1
-		return card
+		if len(self.turned) == 0:
+			return None
+		else:
+			card = self.deck[-1]
+			self.turned = self.turned[:-1]
+			return self.turned[-1]
+		
 
 class Bank_Col:
 	def __init__(self, suit):
@@ -74,129 +86,6 @@ class Game:
 			for val in ['A','2','3','4','5','6','7','8','9','10','J','Q','K','A']:
 				self.deck.append(Card(suit, val)
 
-	def deal(self):
-		random.shuffle(self.deck)
-		deal_point = 0
-		for i in range(7):
-			for j in range(i+1):
-				cols[i].deal(self.deck[deal_point])
-				deal_point += 1
-
-		while deal_point < 52:
-			self.deck_turn.add_card(self.deck[deal_point])
-			deal_point += 1
-
-		for i in range(7):
-			self.cols[i].turn()
-
-		
-	def move_valid_col(self, col_1, col_2):
-		if len(self.cols[col_1].stack) == 0:
-			return False
-		stack_top = self.cols[col_1].stack[0]
-		if len(self.cols[col_2].stack) == 0:
-			if stack_top.val == 'K':
-				return True
-			else:
-				return False
-		stack_bot = self.cols[col_2].stack[-1]
-		if stack_top.colour != stack_bot.colour:
-			if stack_top.num_val + 1 == stack_bot.num_val:
-				return True
-
-		return False:
-
-	def move_valid_deck_col(self, col_2):
-		if self.deck_turn.size > 0:
-			card = self.deck_turn.card
-			if len(self.cols[col_2].stack) == 0:
-				if card.val == 'K':
-					return True
-			else:
-				stack_bot = self.cols[col_2].stack[-1]
-				if card.colour != stack_bot.colour:
-					if card.num_val + 1 == stack_bot.num_val:
-						return True
-		return False
-
-	def move_valid_bank(self, col_1, bank_2):
-		if len(self.cols[col_1].stack) == 0:
-			return False
-		stack_top = self.cols[col_1].stack[0]
-		if stack_top.suit == self.bank[bank_2].suit:
-			if len(self.bank[bank_2]) == 0:
-				if stack_top.val == 'A':
-					return True
-			
-			else:
-				bank_top = self.bank[bank_2].top
-				if stack_top.num_val == bank_top.num_val + 1:
-					return True
-		return False
-
-	def move_valid_deck_bank(self, bank_2):
-		if self.deck_turn.size > 0:
-			card = self.deck_turn.card
-			if card.suit == self.bank[bank_2].suit:
-				if len(self.bank[bank_2]) == 0:
-					if card.val == 'A':
-						return True
-				
-				else:
-					bank_top = self.bank[bank_2].top
-					if card.num_val == bank_top.num_val + 1:
-						return True
-		return False
-
-
-
-	def move_col(self, col_1, col_2):
-		if self.move_valid_col(col_1, col_2):
-			self.cols[col_2].stack += self.cols[col_1].stack
-			self.cols[col_1].stack = []
-			if len(self.cols[col_1].cards_hidden) > 0):
-				self.cols[col_1].turn()
-			
-	def move_bank(self, col_1, bank_2):
-		if self.move_valid_bank(col_1, bank_2):
-			card = self.cols[col_1].stack[-1]
-			self.cols[col_1].stack = self.cols[col_1].stack[:-1]
-			self.bank[bank_2].add_card(card)
-
-	def move_deck_col(self, col_2):
-		if self.move_valid_deck_col(col_2):
-			card = self.deck_turn.take()
-			self.cols[col_2].stack.append(card)
-
-	def move_deck_bank(self, bank_2):
-		if self.move_valid_deck_bank(bank_2):
-			card = self.deck_turn.take()
-			self.bank[bank_2].add_card(card)
-
-	def available_moves(self):
-		move_dict = {'col_moves':[],'bank_moves':[],'deck_col_moves':[],'deck_bank_moves':[],'count':0}
-		for col_1 in range (7):
-			for col_2 in range(7):
-				if self.move_valid_col(col_1, col_2):
-					move_dict['col_moves'].append([col_1,col_2])
-					move_dict['count'] += 1
-
-			for bank_2 in range(4):
-				if self.move_valid_bank(col_1, bank_2):
-					move_dict['bank_moves'].append([col_1,bank_2])
-					move_dict['count'] += 1
-			
-		for col_2 in range(7):
-			if self.move_valid_deck_col(col_2)
-				move_dict['deck_col_moves'].append(col_2)
-				move_dict['count'] += 1
-
-		for bank_2 in range(4):
-			if self.move_valid_deck_bank(bank_2)
-				move_dict['deck_bank_moves'].append(bank_2)
-				move_dict['count'] += 1
-
-		return move_dict
 
 
 		
